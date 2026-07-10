84d02f60373cf78152d09c6596ba2c33a90bf9cb
Alexey <aklukyanov@gmail.com>
2026-07-11 00:16:08 +0300

    + изменен mode для ветки dev

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ dev_base_url="http://127.0.0.1:8000"
 prod_base_url="http://breadlab-server:8000"
 
 #MODE может быть prod или dev
-MODE='prod'
+MODE='dev'
 
 if MODE=='dev':
     CURRENT_URL = dev_base_url
