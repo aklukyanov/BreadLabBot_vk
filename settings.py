0c4051a7f60f73ae70314af369dbfe0ae7ca5e6c
Alexey <aklukyanov@gmail.com>
2026-07-11 14:40:57 +0300

    + фикс настройки окружений через .env

--- settings.py
+++ settings.py
@@ -1,14 +1,13 @@
+import os
+
 dev_base_url="http://127.0.0.1:8000"
 prod_base_url="http://breadlab-server:8000"
 
-#MODE может быть prod или dev
-MODE='prod'
+CURRENT_URL=os.getenv("CURRENT_URL", 'http://127.0.0.1:8000')
+CURRENT_STORAGE=os.getenv("CURRENT_STORAGE", 'ctx_storage')
 
-if MODE=='dev':
-    CURRENT_URL = dev_base_url
-    CURRENT_STORAGE = 'dev'  # 'prod' - redis или 'dev' для разработки
-elif MODE=='prod':
-    CURRENT_URL=prod_base_url
-    CURRENT_STORAGE = 'prod'
+
+
+
 
 
